81a28991dcd18d8ea5eb89537539fdf7391e5192
Espen <espenpede@rocketmail.com>
2022-09-11 16:21:11 +0200

    En gang til

--- js/script.js
+++ js/script.js
@@ -1,53 +1,135 @@
+// console.log("Boklisten");
+// const output = document.querySelector(".output");
+// const sorterÅrKnapp = document.querySelector(".sorterÅr");
+// const sorterTittelKnapp = document.querySelector(".sorterTittel");
+// const sorterNavnOgTittelKnapp = document.querySelector(".sorterNavnOgTittel");
+
+// class bok {
+//     constructor ( f, e, t, å ) {
+//         this.fornavnForfatter = f;
+//         this.etternavnForfatter = e;
+//         this.tittel = t;
+//         this.år = å;
+//     }
+
+//     toString() {
+//         return `${this.fornavnForfatter} ${this.etternavnForfatter}: ${this.tittel} (${this.år})`
+//     }
+
+//     static sammenlignÅr ( a, b ) { return b.år - a.år }
+
+//     static sammenlignTittel ( a, b ) {
+
+//         let aTittel = a.tittel.toLowerCase();
+//         let bTittel = b.tittel.toLowerCase();
+
+//         if (aTittel < bTittel) return -1; 
+//         if (aTittel > bTittel) return 1;
+        
+//         return 0;
+//     }
+
+//     static sammenlignNavnOgTittel ( a, b ) {
+//         let aEtternavn = a.etternavnForfatter.toLowerCase();
+//         let bEtternavn = b.etternavnForfatter.toLowerCase();
+
+//         let aFornavn = a.fornavnForfatter.toLowerCase();
+//         let bFornavn = b.fornavnForfatter.toLowerCase();
+
+//         let aTittel = a.tittel.toLowerCase();
+//         let bTittel = b.tittel.toLowerCase();
+
+//         if (aEtternavn < bEtternavn) return -1;
+//         if (aEtternavn > bEtternavn) return 1;
+
+//         if (aFornavn < bFornavn) return -1;
+//         if (aFornavn > bFornavn) return 1;
+
+//         if (aTittel < bTittel) return -1; 
+//         if (aTittel > bTittel) return 1;
+
+//         return 0;
+//     }
+// }
+
+// const bøker = [
+//     new bok("J. R. R.", "Tolkien", "Ringenes Herre", 1954),
+//     new bok("J. R. R.", "Tolkien", "Hobbiten", 1937),
+//     new bok("J. R. R.", "Tolkien", "Silmarillion", 1977),
+//     new bok("Bill", "Bryson", "A Walk In The Woods", 1998),
+//     new bok("Bill", "Bryson", "Down Under", 2000),
+//     new bok("Yuval Noah", "Harari", "Sapiens: A Brief History of Humankind", 2011),
+//     new bok("Yuval Noah", "Harari", "Homo Deus", 2015),
+//     new bok("Terry", "Hayes", "I Am Pilgrim", 2013),
+//     new bok("George R. R.", "Martin", "A Game of Thrones", 1996),
+//     new bok("George R. R.", "Martin", "A Clash of Kings", 1998),
+// ]
+
+// let listBøker = () => {
+//     output.innerHTML = "";
+
+//     for (let bok of bøker) {
+//         console.log(bok.toString());
+//         output.innerHTML += `<p>${bok.toString()}</p>`;
+//     }    
+// }
+
+// listBøker();
+
+// sorterÅrKnapp.addEventListener("click", () => {
+//     bøker.sort(bok.sammenlignÅr);
+//     listBøker();
+// })
+
+// sorterTittelKnapp.addEventListener("click", () => {
+//     bøker.sort(bok.sammenlignTittel);
+//     listBøker();
+// })
+
+// sorterNavnOgTittelKnapp.addEventListener("click", () => {
+//     bøker.sort(bok.sammenlignNavnOgTittel);
+//     listBøker();
+// })
+
+//-----------------------------
+
 console.log("Boklisten");
 const output = document.querySelector(".output");
 const sorterÅrKnapp = document.querySelector(".sorterÅr");
 const sorterTittelKnapp = document.querySelector(".sorterTittel");
-const sorterNavnOgTittelKnapp = document.querySelector(".sorterNavnOgTittel");
+const sorterNavnOgTittelKnapp = document.querySelector(".sorterNavnOgTittel"); 
 
 class bok {
-    constructor ( f, e, t, å ) {
-        this.fornavnForfatter = f;
-        this.etternavnForfatter = e;
-        this.tittel = t;
-        this.år = å;
+    constructor ( f, l, t, y ) {
+        this.firstName = f,
+        this.lastName = l,
+        this.title = t;
+        this.year = y;
     }
 
     toString() {
-        return `${this.fornavnForfatter} ${this.etternavnForfatter}: ${this.tittel} (${this.år})`
+        return `${this.firstName} ${this.lastName} (${this.title}), ${this.year}`
     }
 
-    static sammenlignÅr ( a, b ) { return b.år - a.år }
+    static sortByYear ( a, b ) {
+        return b.year - a.year
+    }
 
-    static sammenlignTittel ( a, b ) {
-
-        let aTittel = a.tittel.toLowerCase();
-        let bTittel = b.tittel.toLowerCase();
-
-        if (aTittel < bTittel) return -1; 
-        if (aTittel > bTittel) return 1;
-        
+    static sortByTitle ( a, b ) {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
         return 0;
     }
 
-    static sammenlignNavnOgTittel ( a, b ) {
-        let aEtternavn = a.etternavnForfatter.toLowerCase();
-        let bEtternavn = b.etternavnForfatter.toLowerCase();
+    static sortByAuthor ( a, b ) {
+        if (a.lastName < b.lastName) return -1;
+        if (a.lastName > b.lastName) return 1;
 
-        let aFornavn = a.fornavnForfatter.toLowerCase();
-        let bFornavn = b.fornavnForfatter.toLowerCase();
+        if (a.firstName < b.firstName) return -1;
+        if (a.firstName > b.firstName) return 1;
 
-        let aTittel = a.tittel.toLowerCase();
-        let bTittel = b.tittel.toLowerCase();
-
-        if (aEtternavn < bEtternavn) return -1;
-        if (aEtternavn > bEtternavn) return 1;
-
-        if (aFornavn < bFornavn) return -1;
-        if (aFornavn > bFornavn) return 1;
-
-        if (aTittel < bTittel) return -1; 
-        if (aTittel > bTittel) return 1;
-
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
         return 0;
     }
 }
@@ -65,28 +147,29 @@ const bøker = [
     new bok("George R. R.", "Martin", "A Clash of Kings", 1998),
 ]
 
-let listBøker = () => {
+
+const listStuff = () => {
     output.innerHTML = "";
 
     for (let bok of bøker) {
-        console.log(bok.toString());
+        console.log(bok);
         output.innerHTML += `<p>${bok.toString()}</p>`;
     }    
 }
 
-listBøker();
+listStuff();
 
 sorterÅrKnapp.addEventListener("click", () => {
-    bøker.sort(bok.sammenlignÅr);
-    listBøker();
+    bøker.sort(bok.sortByYear);
+    listStuff();
 })
 
 sorterTittelKnapp.addEventListener("click", () => {
-    bøker.sort(bok.sammenlignTittel);
-    listBøker();
+    bøker.sort(bok.sortByTitle);
+    listStuff();
 })
 
 sorterNavnOgTittelKnapp.addEventListener("click", () => {
-    bøker.sort(bok.sammenlignNavnOgTittel);
-    listBøker();
+    bøker.sort(bok.sortByAuthor);
+    listStuff();
 })
